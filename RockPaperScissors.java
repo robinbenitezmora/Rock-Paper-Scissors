4cbdd801fd62d04eae0ce05df8bc835e8c639531
robinbenitezmora <robinbenitez1990@gmail.com>
2023-11-30 20:19:53 -0500

    Create classes User and Computer

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class RockPaperScissors {
  private enum Move {
   ROCK, PAPER, SCISSORS;
+ }
+
+ private class User {
+  private Scanner inputScanner;
+
+  public User() {
+   inputScanner = new Scanner(System.in);
+  }
+
+  public Move getMove() {
+   return null;
+  }
+ }
+
+ private class Computer {
+  public Move getMove() {
+   return null;
+  }
  }
 
  public RockPaperScissors() {
