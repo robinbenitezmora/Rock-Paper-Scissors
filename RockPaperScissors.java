6549092c714714ffc95b1b56c4c9c09f7365cf7a
robinbenitezmora <robinbenitez1990@gmail.com>
2023-11-30 20:38:58 -0500

    Conect the classes User and Computer by means RockPaperScissors class

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -2,57 +2,68 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
- private enum Move {
-  ROCK, PAPER, SCISSORS;
- }
-
- private class User {
-  private Scanner inputScanner;
-
-  public User() {
-   inputScanner = new Scanner(System.in);
+  private enum Move {
+    ROCK, PAPER, SCISSORS;
   }
 
-  public Move getMove() {
-   System.out.print("Rock, Paper or Scissors?");
-   String userInput = inputScanner.nextLine();
-   userInput = userInput.toUpperCase();
-   char firstLetter = userInput.charAt(0);
-   if (firstLetter == 'R' || firstLetter == 'P' || firstLetter == 'S') {
-    switch (firstLetter) {
-     case 'R':
-      return Move.ROCK;
-     case 'P':
-      return Move.PAPER;
-     case 'S':
-      return Move.SCISSORS;
+  private class User {
+    private Scanner inputScanner;
+
+    public User() {
+      inputScanner = new Scanner(System.in);
     }
-   }
-   return getMove();
+
+    public Move getMove() {
+      System.out.print("Rock, Paper or Scissors?");
+      String userInput = inputScanner.nextLine();
+      userInput = userInput.toUpperCase();
+      char firstLetter = userInput.charAt(0);
+      if (firstLetter == 'R' || firstLetter == 'P' || firstLetter == 'S') {
+        switch (firstLetter) {
+          case 'R':
+            return Move.ROCK;
+          case 'P':
+            return Move.PAPER;
+          case 'S':
+            return Move.SCISSORS;
+        }
+      }
+      return getMove();
+    }
+
+    public boolean playAgain() {
+      System.out.print("Do you want to play again?");
+      String userInput = inputScanner.nextLine();
+      userInput = userInput.toUpperCase();
+      return userInput.charAt(0) == 'Y';
+    }
   }
 
-  public boolean playAgain() {
-   System.out.print("Do you want to play again?");
-   String userInput = inputScanner.nextLine();
-   userInput = userInput.toUpperCase();
-   return userInput.charAt(0) == 'Y';
+  private class Computer {
+    public Move getMove() {
+      Move[] moves = Move.values();
+      Random random = new Random();
+      int index = random.nextInt(moves.length);
+      return moves[index];
+    }
   }
- }
 
- private class Computer {
-  public Move getMove() {
-   Move[] moves = Move.values();
-   Random random = new Random();
-   int index = random.nextInt(moves.length);
-   return moves[index];
+  private User user;
+  private Computer computer;
+  private int userScore;
+  private int computerScore;
+  private int numberOfGames;
+
+  public RockPaperScissors() {
+    user = new User();
+    computer = new Computer();
+    userScore = 0;
+    computerScore = 0;
+    numberOfGames = 0;
   }
- }
 
- public RockPaperScissors() {
- }
-
- public static void main(String[] args) {
-  Move move = Move.ROCK;
-  System.out.println(move);
- }
+  public static void main(String[] args) {
+    Move move = Move.ROCK;
+    System.out.println(move);
+  }
 }
