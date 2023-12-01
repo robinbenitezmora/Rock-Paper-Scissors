ee7058f0253ef2dccef961d173ee6f5078bc08dc
robinbenitezmora <robinbenitez1990@gmail.com>
2023-11-30 20:25:38 -0500

    Create getMove method in User class

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -14,7 +14,21 @@ public class RockPaperScissors {
   }
 
   public Move getMove() {
-   return null;
+   System.out.print("Rock, Paper or Scissors?");
+   String userInput = inputScanner.nextLine();
+   userInput = userInput.toUpperCase();
+   char firstLetter = userInput.charAt(0);
+   if (firstLetter == 'R' || firstLetter == 'P' || firstLetter == 'S') {
+    switch (firstLetter) {
+     case 'R':
+      return Move.ROCK;
+     case 'P':
+      return Move.PAPER;
+     case 'S':
+      return Move.SCISSORS;
+    }
+   }
+   return getMove();
   }
  }
 
