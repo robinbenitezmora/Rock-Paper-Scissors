6186d638baae9278511f4e3a100eec1f75b09703
robinbenitezmora <robinbenitez1990@gmail.com>
2023-11-30 20:14:04 -0500

    Create the constants ROCK, PAPER and SCISSORS

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,7 +1,13 @@
 public class RockPaperScissors {
+ private enum Move {
+  ROCK, PAPER, SCISSORS;
+ }
+
  public RockPaperScissors() {
  }
 
  public static void main(String[] args) {
+  Move move = Move.ROCK;
+  System.out.println(move);
  }
 }
