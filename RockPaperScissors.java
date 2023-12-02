9384986eea584d1dd5def61e57375e252963970b
robinbenitezmora <robinbenitez1990@gmail.com>
2023-12-01 21:22:43 -0500

    Add 'compareMoves'  method

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 public class RockPaperScissors {
   private enum Move {
     ROCK, PAPER, SCISSORS;
+
+    public int compareMoves(Move otherMove) {
+      // Tie
+      if (this == otherMove)
+        return 0;
+
+      switch (this) {
+        case ROCK:
+          return (otherMove == SCISSORS ? 1 : -1);
+        case PAPER:
+          return (otherMove == ROCK ? 1 : -1);
+        case SCISSORS:
+          return (otherMove == PAPER ? 1 : -1);
+      }
+
+      // Should never reach here
+      return 0;
+    }
   }
 
   private class User {
