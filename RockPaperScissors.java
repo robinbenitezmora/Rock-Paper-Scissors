80975fabdad84cc686688b724168d9443f6ecaf7
robinbenitezmora <robinbenitez1990@gmail.com>
2023-12-01 21:51:55 -0500

    Move startGame and create printGameStats

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -22,15 +22,6 @@ public class RockPaperScissors {
       // Should never reach here
       return 0;
     }
-
-    public void startGame() {
-      System.out.println("ROCK, PAPER, SCISSORS");
-      Move userMove = user.getMove();
-      Move computerMove = computer.getMove();
-      System.out.println("\nYour move " + userMove + ".");
-      System.out.println("Computer move " + computerMove + ".\n");
-    }
-
   }
 
   private class User {
@@ -87,6 +78,82 @@ public class RockPaperScissors {
     userScore = 0;
     computerScore = 0;
     numberOfGames = 0;
+  }
+
+  public void startGame() {
+    System.out.println("ROCK, PAPER, SCISSORS");
+    Move userMove = user.getMove();
+    Move computerMove = computer.getMove();
+    System.out.println("\nYour move " + userMove + ".");
+    System.out.println("Computer move " + computerMove + ".\n");
+
+    int compareMoves = userMove.compareMoves(computerMove);
+
+    switch (compareMoves) {
+      case 0: // Tie
+        System.out.println("Tie!");
+        break;
+      case 1: // User wins
+        System.out.println(userMove + " beats " + computerMove + ". You won!");
+        userScore++;
+        break;
+      case -1: // Computer wins
+        System.out.println(computerMove + " beats " + userMove + ". You lost.");
+        computerScore++;
+        break;
+    }
+    numberOfGames++;
+
+    if (user.playAgain()) {
+      System.out.println();
+      startGame();
+    } else {
+      printGameStats();
+    }
+  }
+
+  private void printGameStats() {
+    int wins = userScore;
+    int losses = computerScore;
+    int ties = numberOfGames - userScore - computerScore;
+    double percentageWon = (wins + ((double) ties) / 2) / numberOfGames;
+
+    // Line
+    System.out.print("+");
+    printDashes(68);
+    System.out.println("+");
+
+    // Print titles
+    System.out.printf("| %6s | %6s | %6s | %12s | %14s |\n",
+        "WINS", "LOSSES", "TIES", "GAMES PLAYED", "PERCENTAGE WON");
+
+    // Line
+    System.out.print("|");
+    printDashes(10);
+    System.out.print("+");
+    printDashes(10);
+    System.out.print("+");
+    printDashes(10);
+    System.out.print("+");
+    printDashes(16);
+    System.out.print("+");
+    printDashes(18);
+    System.out.println("|");
+
+    // Print values
+    System.out.printf("| %6d | %6d | %6d | %12d | %13.2f%% |\n",
+        wins, losses, ties, numberOfGames, percentageWon * 100);
+
+    // Line
+    System.out.print("+");
+    printDashes(68);
+    System.out.println("+");
+  }
+
+  private void printDashes(int numberOfDashes) {
+    for (int i = 0; i < numberOfDashes; i++) {
+      System.out.print("-");
+    }
   }
 
   public static void main(String[] args) {
